818023aa0b0204eaf8ca2ab4a57f51465b32bd9d
Luckman Ali <luckman.ali@live.com>
2021-02-03 18:09:22 +0000

    created tests for the scheduler

---
+++ tests/test_schedule_adhan.py
@@ -0,0 +1,32 @@
+from adhan_scheduler.schedule_adhan import ScheduleAdhan
+
+
+def test_schedule_single_job():
+    with ScheduleAdhan(prayer_times={"Asr": "14:30"}, command='echo hello_world') as cron:
+        assert cron.get_job("Asr") == {
+            'time': '30 14 * * *',
+            'command': 'echo hello_world',
+            'comment': 'Asr'
+        }
+
+    # Make sure job has been deleted
+    assert cron.get_job("Asr") is None
+
+
+def test_schedule_multiple_jobs():
+    with ScheduleAdhan(prayer_times={"Maghrib": "17:29", "Isha": "19:00"}, command='echo hello_world') as cron:
+        assert cron.get_job("Maghrib") == {
+            'time': '29 17 * * *',
+            'command': 'echo hello_world',
+            'comment': 'Maghrib'
+        }
+
+        assert cron.get_job("Isha") == {
+            'time': '0 19 * * *',
+            'command': 'echo hello_world',
+            'comment': 'Isha'
+        }
+
+    # Make sure jobs have been deleted
+    assert cron.get_job("Maghrib") is None
+    assert cron.get_job("Isha") is None
